d67a685df96a0f0b97f8f9cfef9f126ac3250e1b
bato <spasekametal1269@icloud.com>
2021-09-29 13:52:33 +0900

    test

--- gauss.c
+++ gauss.c
@@ -13,12 +13,22 @@ int main(){
         }
     }
     
+    float b=-gyouretu[2][1]/gyouretu[1][1];
+    for(int i=0; i<4;i++){
+            gyouretu[2][i]=gyouretu[1][i]*b+gyouretu[2][i];
+    }
+
+    float x3=gyouretu[2][3]/gyouretu[2][2];
+    float x2=(gyouretu[1][3]-gyouretu[1][2]*x3)/gyouretu[1][1];
+    float x1=(gyouretu[0][3]-x2-gyouretu[0][2]*x3)/gyouretu[0][0];
+
     for(int m=0;m<3;m++){
         for(int i=0;i<4;i++){
             printf("%f,",gyouretu[m][i]);
         }
         printf("\n");
     }
+    printf("%f,%f,%f\n",x1,x2,x3);
    // b=gyouretu[0][1]*a+gyouretu[1][1];
     
     return 0;
