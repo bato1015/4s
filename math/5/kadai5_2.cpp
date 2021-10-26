3303869ee83456738a93dc9de46692fae1229c0e
Bato Riko <s20c1101jd@s.chib>
2021-10-26 20:56:32 +0900

    test

--- math/5/kadai5_2.cpp
+++ math/5/kadai5_2.cpp
@@ -3,13 +3,13 @@
 
 //2x^2+3xの解
 float f1(float x){
-    return 2*x*x+3*x;
+    return 2*x*x+3;
 }
 float df1(float x){
-    return 4*x+3;
+    return 4*x;
 }
 float Newton(float a){
-    float x=0;
+    float x;
     x=a-f1(a)/df1(a); //微分のずれなされている
     printf("%lf\n",x);
     return x;
@@ -19,8 +19,8 @@ int i=0;
 float a=3;
     do{
       a= Newton(a);
-   
-    }(i<14)
+      i++;
+    }while(i<14);
 
     return 0;
 }
