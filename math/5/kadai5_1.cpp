8f32fc817416c1947ff420576c03e86c0865f7cb
Bato Riko <s20c1101jd@s.chib>
2021-10-26 22:49:09 +0900

    test

--- math/5/kadai5_1.cpp
+++ math/5/kadai5_1.cpp
@@ -26,11 +26,11 @@ double bibun(double a){ //微分
 bool hantei(int i,double a){ //収束確認
     const double mnum=1.0e-10;
     const int i_max=100;
-    if(i>i_max){
+    if(i>i_max){//最大100回まで計算をする
         printf("error");
         return false;
-    }else if(mnum>fabs(delta(a))){
-        printf("%d回で収束　答え%lf\n",i,a);
+    }else if(mnum>fabs(delta(a))){//1.0e-10の範囲に入るまで計算する
+        printf("%d回で収束　答え%lf\n",i,a);//収束回数の表示　
         return false;
     }
     return true;
@@ -40,8 +40,8 @@ void keisan(double a){ //ニュートン法
     
      do {
             i++;
-            a+=delta(a);
-            printf("%d回 x:%lf\n",i,a);
+            a+=delta(a);//f(x)=0の解を求める
+            printf("%d回 x:%lf\n",i,a);//解の変位を表示する
         }while(hantei(i,a));
 
    // printf("\n");    
